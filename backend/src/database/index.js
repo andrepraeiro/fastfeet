918ea9446cbe11a28c966539162d5d37e7520b60
Andre Praeiro <andrepraeiro@gmail.com>
2020-03-18 17:48:59 -0400

    config sequelize

---
+++ backend/src/database/index.js
@@ -0,0 +1,20 @@
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const Sequelize = require('sequelize');
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
