03b6466c5cda6df468232dfbaf1947a2b311cd12
Andre Praeiro <andrepraeiro@gmail.com>
2020-04-06 23:40:44 -0400

    first test

---
+++ backend/src/tests/user.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Create user endpoint', () => {
+  it('should create a new user', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send({
+        name: 'userteste',
+        email: 'userteste@fastfeet.com',
+        password: '123456',
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      email: 'userteste@fastfeet.com',
+      id: 1,
+      name: 'userteste',
+    });
+  });
+  it('should emit a validation error', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send({
+        email: 'userteste@fastfeet.com',
+        password: '123456',
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+  it('should emit user already exists error', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send({
+        name: 'userteste',
+        email: 'userteste@fastfeet.com',
+        password: '123456',
+      });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({
+      error: 'User already exists',
+    });
+  });
+});
